c86be50d27db01e42de2528915dab8ca3f24da87
Nikolay Dimitrov <nikidimi@gmail.com>
2017-02-26 18:08:12 +0200

    Added training module

---
+++ train.py
@@ -0,0 +1,62 @@
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+import glob
+import pickle
+from skimage.feature import hog
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from multiprocessing.dummy import Pool as ThreadPool 
+from sklearn.svm import SVC, LinearSVC
+from sklearn.metrics import accuracy_score
+
+def get_hog(image_channel):
+    orient = 12
+    pix_per_cell = 4
+    cell_per_block = 2
+    return hog(image_channel, orient, (pix_per_cell, pix_per_cell),
+               (cell_per_block, cell_per_block), feature_vector=True)\
+               
+def get_image_features(image_path):
+    features = [];
+    
+    image = mpimg.imread(image_path)
+    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    features.append(get_hog(hsv[:, :, 0]))
+    features.append(get_hog(hsv[:, :, 1]))
+    features.append(get_hog(hsv[:, :, 2]))
+
+    return np.concatenate(features)
+
+
+def prepare_data(car_images, non_car_images):
+    pool = ThreadPool(4) 
+    X_car = pool.map(get_image_features, car_images, 4)
+    Y_car = [1] * len(X_car)
+    X_non_car = pool.map(get_image_features, non_car_images, 4)
+    Y_non_car = [0] * len(X_non_car)
+
+    X = X_car + X_non_car
+    Y = Y_car + Y_non_car
+    return X, Y
+
+def train(X, Y):
+    X, Y = shuffle(X, Y)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33)
+    clf = LinearSVC()
+    clf.fit(X_train, y_train) 
+    y_pred = clf.predict(X_test)
+    print("Accuracy:{}".format(accuracy_score(y_test, y_pred)))
+    
+    with open('clf.pickle', 'wb') as handle:
+        pickle.dump(clf, handle)
+    
+if __name__ == "__main__":
+    car_images = glob.glob('train/vehicles/*/*.png')
+    non_car_images = glob.glob('train/non-vehicles/*/*.png')
+
+    car_images = shuffle(car_images)
+    non_car_images = shuffle(non_car_images)    
+
+    X, Y = prepare_data(car_images, non_car_images)
+    train(X, Y)
