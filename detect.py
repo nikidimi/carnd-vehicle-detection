a1c9235ba8217bc40e674109249f65e6c48fd36f
Nikolay Dimitrov <nikidimi@gmail.com>
2017-03-06 00:23:17 +0200

    detection phase added

---
+++ detect.py
@@ -0,0 +1,127 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import glob
+from skimage.feature import hog
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+from multiprocessing.dummy import Pool as ThreadPool 
+from scipy.ndimage.measurements import label
+from sklearn.svm import SVC, LinearSVC
+import pickle
+from moviepy.editor import VideoFileClip, ImageSequenceClip
+from common import get_image_features, get_hog, bin_spatial, color_hist, get_image_features_arr
+
+heatmaps = []
+
+def calc_rect(image, rect, scale):
+    global clf
+
+    scaled = cv2.resize(image, (np.int(image.shape[1]/scale), np.int(image.shape[0]/scale)))
+    scaled = (scaled/255.0).astype('float32')
+    hsv = cv2.cvtColor(scaled, cv2.COLOR_RGB2HSV)
+    
+    h1 = get_hog(hsv[:, :, 0], feature_vector=False)
+    h2 = get_hog(hsv[:, :, 1], feature_vector=False)
+    h3 = get_hog(hsv[:, :, 2], feature_vector=False)
+    
+    
+    for x in range(0, h1.shape[0] - 7, 2):
+        for y in range(0, h1.shape[1] - 7, 2):
+            subimage = hsv[(x * 8): (x * 8) + 64, (y * 8): (y * 8) + 64, :]
+            
+            features = []
+            features.append(h1[x:x + 7, y:y + 7].ravel())
+            features.append(h2[x:x + 7, y:y + 7].ravel())
+            features.append(h3[x:x + 7, y:y + 7].ravel())
+            features.append(bin_spatial(subimage))
+            features.extend(color_hist(subimage))
+            
+            data = np.concatenate(features)
+            scaled_data = X_scaler.transform(data.reshape(1, -1))
+            
+            result = clf.predict(scaled_data)
+
+            if result[0] > 0 and clf.decision_function(scaled_data) > 0.6:
+                rect.append(((np.int(y * 8 * scale), 
+                              np.int(360 + x * 8 * scale)), 
+                             (np.int((y+7) * 8 * scale), 
+                              np.int(360 + (x+7) * 8 * scale))))
+
+def draw_labeled_bboxes(image, labels):
+    img = np.copy(image)
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+
+def parse_image(image):
+    global heatmaps     
+    output = np.empty((1440, 2560, 3), dtype='uint8')
+    
+    bboxes = []   
+    heatmap = np.zeros_like(image[:,:,0]).astype(np.float)
+    
+    for scale in [1.0, 1.5, 2.0, 3.0]:
+        calc_rect(image[360:720], bboxes, scale)
+        
+    for box in bboxes:
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+        
+        
+    heatmaps.append(heatmap)
+    if len(heatmaps) > 10:
+        heatmaps.pop(0)
+    
+    summed = np.sum(heatmaps, 0)
+    threshold_heatmap = np.copy(summed)
+    threshold_heatmap[threshold_heatmap <= 5] = 0
+    labels = label(threshold_heatmap)
+    
+            
+    output[0:720, 0:1280] = draw_labeled_bboxes(image, labels)
+    
+    output[720:1440, 0:1280, 0] = summed * 10
+    output[720:1440, 0:1280, 1] = summed * 10
+    output[720:1440, 0:1280, 2] = summed * 10
+    
+    output[720:1440, 1280:2560, 0] = heatmap * 10
+    output[720:1440, 1280:2560, 1] = heatmap * 10
+    output[720:1440, 1280:2560, 2] = heatmap * 10
+    
+    output[0:720, 1280:2560] = draw_boxes(image, bboxes)
+    
+    return output
+
+if __name__ == "__main__":
+    global clf, X_scaler
+    
+    with open('clf.pickle', 'rb') as handle:
+        clf = pickle.load(handle)
+    with open('scaler.pickle', 'rb') as handle:
+        X_scaler = pickle.load(handle)
+    
+    clip = VideoFileClip("project_video.mp4")
+    clip = clip.subclip(40, 41)
+    processed_clip = clip.fl_image(parse_image)
+    processed_clip.write_videofile("out.mp4", audio=False)
---
+++ writeup_template.md
@@ -0,0 +1,63 @@
+
+# Rubric Points
+
+
+### Histogram of Oriented Gradients (HOG)
+
+##### 1. Explain how (and identify where in your code) you extracted HOG features from the training images.
+
+The code for extracting various features is located in common.py
+
+Extraction of HOG features is located in the get_hog function
+
+I'm using the hog function from skimage.features.
+
+#### 2. Explain how you settled on your final choice of HOG parameters.
+
+The parameters and the color space were chosen after experimenting with different values. Using this configuration resulted in the highest accuracy
+
+#### 3. Describe how (and identify where in your code) you trained a classifier using your selected HOG features (and color features if you used them).
+
+The training of the classifier is done in train.py.
+First, I read all the images and extract the features from them. This done in multiple processes in order to speed-up the training.
+After that the features are scaled using StandardScaler from sklearn.
+The final step is to run the classifier. I'm using the SVC linear classifier
+In the end, the trained classifier and the scaler are saved to files
+
+### Sliding Window Search
+
+#### 1. Describe how (and identify where in your code) you implemented a sliding window search.  How did you decide what scales to search and how much to overlap windows?
+
+The sliding window is implemented in the function calc_rect in detect.py
+
+First, I calculate the HOG features for the whole image. After that, I iterate through the result with a step of 2. In order to get the corresponding subimage to the hog features, I multiple the x and y coordinates by the pixels per cell values - 8.
+
+I've tried different scales and it end decided to keep 1.0, 1.5, 2.0 and 3.0. They do a reasonable job of finding cars in different positions. I always tried different steps and 2 seems to be an optimal between not having too many duplicates and capturing all the cars.
+
+#### 2. Show some examples of test images to demonstrate how your pipeline is working.  What did you do to optimize the performance of your classifier?
+
+I've tuned the parameters of the feature extractors. I've also added decision function threshold.
+
+### Video Implementation
+
+#### 1. Provide a link to your final video output.  Your pipeline should perform reasonably well on the entire project video (somewhat wobbly or unstable bounding boxes are ok as long as you are identifying the vehicles most of the time with minimal false positives.)
+Here's a [link to my video result](./out_aws.mp4)
+The upper-left corner of the video is the final result.
+The upper-right corner show the results before applying the heatmap.
+In the bottom, the heatmap itself is displayed
+
+#### 2. Describe how (and identify where in your code) you implemented some kind of filter for false positives and some method for combining overlapping bounding boxes.
+
+Overlapping bounding boxes are combined by using a heatmap. I retain the last 10 heatmaps and combine them in order to smooth the results.
+This is implemented in the function parse_image in detect.py
+
+False positives are filtered by thresholding the decision function and by thresholding the heatmap
+
+
+### Discussion
+
+#### 1. Briefly discuss any problems / issues you faced in your implementation of this project.  Where will your pipeline likely fail?  What could you do to make it more robust?
+
+The training of the classifier is rather slow and tuning all the parameters takes a lot of trail and error.
+It is possible to make it more robust by adding some kind of tracking for the cars - the can only move a few pixels per frame, their scale should be determined by their position in the frame and they can't just pop up in the middle of the road. By checking for all this conditions, we can remove false positives.
+We can also make a more rigorous search (by reducing the classifier threshold, searching with more scales and etc) if a car suddenly disappears.
